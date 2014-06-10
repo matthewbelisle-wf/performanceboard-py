1616072dc80d3e290513e083015f5d8b46ffcbff
Matt Belisle <mgbelisle@gmail.com>
2014-06-09 20:21:56 -0600

    Nesting metrics

--- performanceboard/__init__.py
+++ performanceboard/__init__.py
@@ -1,12 +1,14 @@
+from collections import defaultdict
 from importlib import import_module
 from time import time
 from traceback import extract_stack
+import atexit
+import inspect
+import logging
+import json
 import os
 
-from performanceboard.plugins import get_plugin
-
-
-PLUGIN = get_plugin()
+import requests
 
 
 class Metric(object):
@@ -14,14 +16,26 @@ class Metric(object):
     Context manager for a PerformanceBoard metric.  Metrics can be hierarchical.
     """
     def __init__(self, key, url=None):
-        self.data = {'key': key}
+        self.key = key
         self.url = url or os.environ.get('PERFORMANCEBOARD_URL')
+        self.metric = defaultdict(list, [('key', key)])
 
     def __enter__(self):
-        self.data['start'] = time()
+        self.metric['start'] = time()
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
-        self.data['end'] = time()
-        if PLUGIN and self.url:
-            PLUGIN.post(data=self.data, url=self.url)
+        self.metric['end'] = time()
+        if self.url:
+            # Nested metrics have their metric attached to the parent
+            for frame in inspect.stack()[1:]:
+                parent = frame[0].f_locals.get('self')
+                if isinstance(parent, Metric):
+                    parent.metric[self.key].append(self.metric)
+                    break
+            else:
+                self.post()
+                logging.info('Posted to {}'.format(self.url))
+
+    def post(self):
+        atexit.register(requests.post, self.url, data=json.dumps([self.metric]))
--- performanceboard/appengine.py
+++ performanceboard/appengine.py
@@ -1,18 +1,12 @@
-from performanceboard.plugins import PluginNotAvailable
-try:
-    import dev_appserver
-except ImportError:
-    raise PluginNotAvailable('Google Appengine')
+import dev_appserver
+
+from . import Metric
+
+
 dev_appserver.fix_sys_path()
 
 
-from google.appengine.ext.deferred import defer
-import requests
-
-
-def _post(data=None, url=None):
-    requests.post(url, data=data)
-
-
-def post(data=None, url=None):
-    defer(_post, data=data, url=url)
+class AppengineMetric(Metric):
+    # TODO: Collect memory metrics
+    pass
+        
