8e849e9c00ac5ee8b3449f3e8b6549d15642b89a
Matt Belisle <mgbelisle@gmail.com>
2014-06-09 23:31:42 -0600

    Making unit tests work

--- performanceboard/__init__.py
+++ performanceboard/__init__.py
@@ -1,9 +1,7 @@
 from collections import defaultdict
 from importlib import import_module
 from time import time
-from traceback import extract_stack
 import atexit
-import inspect
 import logging
 import json
 import os
@@ -18,24 +16,26 @@ class Metric(object):
     def __init__(self, key, url=None):
         self.key = key
         self.url = url or os.environ.get('PERFORMANCEBOARD_URL')
-        self.metric = defaultdict(list, [('key', key)])
+        self.data = defaultdict(list, [('key', key)])
 
     def __enter__(self):
-        self.metric['start'] = time()
+        self._stack.append(self)
+        self.data['start'] = time()
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
-        self.metric['end'] = time()
-        if self.url:
-            # Nested metrics have their metric attached to the parent
-            for frame in inspect.stack()[1:]:
-                parent = frame[0].f_locals.get('self')
-                if isinstance(parent, Metric):
-                    parent.metric[self.key].append(self.metric)
-                    break
-            else:
-                self.post()
-                logging.info('Posted to {}'.format(self.url))
+        self._stack.pop()
+        self.data['end'] = time()
+        if self._stack:
+            parent = self._stack[-1]
+            parent.data[self.key].append(self.data)
+        else:
+            self.post()
+            logging.info('Posted to {}'.format(self.url))
+
+    # Since nested metrics are all bundled into one post, `_stack` keeps track of the call chain to
+    # enable accessing the parent metric.
+    _stack = []
 
     def post(self):
-        atexit.register(requests.post, self.url, data=json.dumps([self.metric]))
+        atexit.register(requests.post, self.url, data=json.dumps([self.data]))
--- performanceboard/tests/unit/test_metric.py
+++ performanceboard/tests/unit/test_metric.py
@@ -1,26 +1,57 @@
 from unittest import TestCase
 
 from mock import patch
+from mock import Mock
 
 import performanceboard
 
 
+@patch('performanceboard.requests', Mock())
 class MetricTestCase(TestCase):
+    def test_post(self):
+        """
+        Tests that post is called exactly once, even with nested metrics.
+        """
+        with patch('performanceboard.Metric.post') as post:
+            with performanceboard.Metric('foo', url='dummy_url') as metric:
+                with performanceboard.Metric('bar', url='dummy_url') as metric2:
+                    pass
+            self.assertEqual(post.call_count, 1)
+
     def test_data(self):
-        url = 'dummy_url'
-        key = 'dummy_key'
-        with patch('performanceboard.PLUGIN') as mock_plugin,\
-             patch.dict('performanceboard.os.environ', {'PERFORMANCEBOARD_URL': url}):
-            with performanceboard.Metric(key) as metric:
-                data = metric.data
-            mock_plugin.post.assert_called_once_with(data=data, url=url)
+        """
+        Tests that metrics can be nested.
+
+        {
+          'key': 'foo',
+          'start': 1402378113.505838,
+          'end': 1402378113.505866,
+          'bar': [
+            {
+              'key': 'bar',
+              'start': 1402378113.50585,
+              'end': 1402378113.505854
+            }
+          ]
+        }
+        """
+        key = 'foo'
+        key2 = 'bar'
+        with performanceboard.Metric(key) as metric:
+            with performanceboard.Metric(key2) as metric2:
+                data2 = metric2.data
+            data = metric.data
         self.assertIn('key', data)
         self.assertIn('start', data)
         self.assertIn('end', data)
+        self.assertIn(data2, data[key2])
 
     def test_url(self):
+        """
+        Tests that os.environ['PERFORMANCEBOARD_URL'] is used as the URL.
+        """
         url = 'dummy_url'
-        with patch('performanceboard.PLUGIN') as mock_plugin:
-            with performanceboard.Metric('dummy_key', url=url) as metric:
-                data = metric.data
-            mock_plugin.post.assert_called_once_with(data=data, url=url)
+        with patch.dict('performanceboard.os.environ', {'PERFORMANCEBOARD_URL': url}),\
+             performanceboard.Metric('foo') as metric:
+            url2 = metric.url
+        self.assertEqual(url, url2)
